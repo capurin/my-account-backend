0c049192b73fcb790e36cd71f49255cb3af1b40f
dante-delivered <dante@delivered.co.kr>
2024-05-08 23:46:34 +0900

    init

---
+++ src/test/kotlin/com/acoount/book/accountbook/AccountBookApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.acoount.book.accountbook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccountBookApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
